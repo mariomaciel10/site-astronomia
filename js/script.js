556c84c820de1e9dcd2784209b2c98b7dc37adc0
Mario Pietro <piedromario7@gmail.com>
2026-03-30 19:44:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.querySelector('.search-text')
+    const sections = document.querySelectorAll('div[id]')
+
+ 
+    if (searchInput) {
+        searchInput.addEventListener('input', (e) => {
+            const term = e.target.value.toLowerCase().trim()
+
+            sections.forEach(section => {
+                const content = (
+                    section.textContent + section.id
+                ).toLowerCase()
+
+                section.style.display =
+                    (!term || content.includes(term)) ? 'block' : 'none'
+            })
+        })
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.onclick = e => {
+    e.preventDefault()
+
+    document.querySelectorAll('div[id]').forEach(s => s.style.display = 'none')
+
+    const alvo = document.querySelector(link.getAttribute('href'))
+    if (alvo) alvo.style.display = 'block'
+  }
+})
+                })
+
